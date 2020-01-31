11a1ef2a526aae27c3ca49941cd601201ddb7555
Aida Abdikulova <abdikulova@gmail.com>
2020-01-31 04:55:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -79,6 +79,8 @@ class Student
 
   def self.all_students_in_grade_X(num)
     sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = ?
     SQL
     DB[:conn].execute(sql,num).map do |row|
       self.new_from_db(row)
