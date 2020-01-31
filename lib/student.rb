9bb74a1df2bbbcd35e12c691a5d3b1202de342ab
Aida Abdikulova <abdikulova@gmail.com>
2020-01-31 04:45:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,6 +68,9 @@ class Student
 
   def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = 10
+    LIMIT num 
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
