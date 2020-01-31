ae4449a5b271eaffe82f81209a207a5d26380cae
Aida Abdikulova <abdikulova@gmail.com>
2020-01-31 04:50:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,9 +70,10 @@ class Student
     sql = <<-SQL
     SELECT * FROM students
     WHERE grade = 10
-    LIMIT num
+    LIMIT ?
+    SELECT
     SQL
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, num).map do |row|
       self.new_from_db(row)
     end
   end
